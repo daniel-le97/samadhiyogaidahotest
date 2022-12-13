7e14b264b7aa23c207102824e1245e430393e0d3
tri le <letrin93@gmail.com>
2022-12-12 20:07:06 -0700

    tri

--- samadhiyogaidaho.client/src/pages/AboutPage.vue
+++ samadhiyogaidaho.client/src/pages/AboutPage.vue
@@ -1,54 +1,55 @@
 <template>
   <div class="">
-         <div class="tsparticles">
-        <ParticleTest />
-      </div>
-    <div
-
-      class="heroImage d-flex flex-column align-items-center justify-content-center"
-    >
+    <div class="tsparticles">
+      <ParticleTest />
+    </div>
+    <div class="heroImage d-flex flex-column align-items-center justify-content-center">
       <div class="text-center">
-        <h1
-          v-motion-slide-top
-          id="shadow1"
-          class="text-shadow display-1 courgette fw-bolder text-light"
-        >
+        <h1 v-motion-slide-top id="shadow1" class="text-shadow display-1 courgette fw-bolder text-light">
           OUR STORY
         </h1>
+        <h3 class="text-shadow">Origin</h3>
       </div>
       <div class="w-75">
-  <p 
-        v-motion-slide-bottom
-      class="text-light px-5 text-start fs-3 text-shadow px-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ipsum
-        error libero voluptatum aliquid, ea adipisci sed ipsa totam voluptate
-        est fugiat maiores hic rem quo deleniti ex! Ea expedita iste cumque,
-        praesentium vitae accusantium at eveniet deserunt facilis soluta
-        asperiores ratione perspiciatis voluptas quaerat perferendis autem culpa
-        pariatur molestias quo earum. Nostrum dolor voluptate consequatur ntium porro, aliquam blanditiis repudiandae facere numquam quam
-        nisi reprehenderit veniam. Similique accusamus harum facere nam libero,
-        vero nostrum, necessitatibus fuga dignissimos facilis asperiores
-        officiis aspernatur, cupiditate quibusdam veritatis nesciunt qui soluta?
-        Magnam non commodi optio doloribus recusandae fugit reiciendis porro
-        soluta quae officia mollitia, sed molestias vero animi impedit atque
-        enim, iste deserunt? Quo necessitatibus velit voluptatum voluptas dolor
-        possimus accusamus obcaecati consectetur!
-      </p>
+        <p v-motion-slide-bottom class="text-light px-5 text-start fs-3 text-shadow px-5">
+          Back in 2013, our founder took her first yoga class in the hopes of finding some healing. Almost 10 years
+          later, she
+          took the initiative to receive her 200-hour training and found her passion in teaching. She realized that
+          people want
+          (and need) the time and space to push pause on their daily routine. They need a safe space for healing,
+          breathing, and
+          feeling. That is why Samadhi Yoga Idaho was created, so our community of participants can self-reflect and
+          reconnect
+          with themselves in a beautiful and serene atmosphere.
+
+          Physical and mental wellbeing are of utmost importance to living a healthy life and require practice and
+          dedication. Our
+          Small Business provides the setting and guidance you need to strengthen your body, mind and soul. All you have
+          to do is
+          show up and breathe.
+        </p>
       </div>
-    
+
     </div>
     <div class="container-fluid mt-5">
 
-         <div class="row mt-5 px-3">
+      <div class="row mt-5 px-3">
         <div class="col-md-6 elevation-6">
-       <img src="https://images.unsplash.com/photo-1593358578872-4772d9cd7a97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="" class="img-fluid framed ">
+          <img
+            src="https://scontent.fboi1-1.fna.fbcdn.net/v/t39.30808-6/306519061_129159436532870_6494007048333346057_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=ZyXKoZ6J5CIAX-iRhfY&_nc_ht=scontent.fboi1-1.fna&oh=00_AfCPQrcs1-iLVW0CLPjhl0-intGdQOE1_KoCweThnqKaAg&oe=639D65A6"
+            alt="" class="img-fluid framed my-5 ms-5 ">
         </div>
         <div class="col-md-6">
-       <p class="display-3"> Jane Doe</p>
-            <p class="text-start p-2 fs-4 mt-md-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ex, dolore eos consequatur veniam cupiditate vel a impedit maxime perferendis aut explicabo ut, nesciunt vero earum nulla et non. Officiis provident impedit voluptatem dolor facere voluptatibus rem eaque molestias, recusandae minima sed ratione saepe quibusdam illum. Hic atque, excepturi eos quisquam esse quia obcaecati animi provident iusto iure quidem fuga, eius officia consectetur. Accusantium facere tempore nulla et officia sit unde consectetur eos maxime deleniti id esse adipisci quas obcaecati alias sunt nisi iste, in dolorem placeat beatae rem. Expedita reiciendis corrupti pariatur dolore saepe alias reprehenderit aut nisi unde explicabo nulla ipsam nam, sequi sunt. Nesciunt repellat incidunt eos unde nihil itaque, doloribus eaque error ipsam, similique nemo neque. Odio id explicabo dicta quae eligendi .</p>
+          <p class="display-3"> Katy Alverson</p>
+          <p class="display-5">Founder, owner and teacher</p>
+          <p class="text-start p-2 fs-4 mt-md-5">Katie believes that to question, to inquire, to seek understanding is a
+            core part of what it means to be human. Whether
+            its at home or away on a retreat, they are always inspired by the experiences of life. She believes the core
+            of a
+            healthy person is in the wellbeing of their body, mind, and soul.</p>
         </div>
-  
-   
+
+
       </div>
     </div>
   </div>
@@ -66,7 +67,6 @@ export default {
 };
 </script>
 <style scoped>
-
 .heroImage {
   height: 100vh;
   /* always scale the image to the appropriate size of your screen */
@@ -76,12 +76,12 @@ export default {
   /* keeps the image fixed while scrolling , neat effect. */
   background-attachment: fixed;
 }
-.framed {
 
- 
+.framed {
+  height: 40em;
+
   box-shadow:
     -50px -50px 0 -40px var(--darkPink),
-    50px 50px 0 -40px  var(--darkPink);
+    50px 50px 0 -40px var(--darkPink);
 }
-
 </style>
