6f4da7b9236ae9aa3ba4e32626c0cba063f0d71c
tri le <letrin93@gmail.com>
2022-12-13 15:06:40 -0700

    tri

--- samadhiyogaidaho.client/src/pages/AboutPage.vue
+++ samadhiyogaidaho.client/src/pages/AboutPage.vue
@@ -11,7 +11,7 @@
         <h3 class="text-shadow">Origin</h3>
       </div>
       <div class="w-75">
-        <p v-motion-slide-bottom class="text-light px-md-5 story-text my-3 text-start  text-shadow ">
+        <p v-motion-slide-bottom class="text-light px-md-5 story-text my-3 text-center  text-shadow ">
           Back in 2013, our founder took her first yoga class in the hopes of finding some healing. Almost 10 years
           later, she
           took the initiative to receive her 200-hour training and found her passion in teaching. She realized that
@@ -21,23 +21,22 @@
           feeling. That is why Samadhi Yoga Idaho was created, so our community of participants can self-reflect and
           reconnect
           with themselves in a beautiful and serene atmosphere.
-
-          Physical and mental wellbeing are of utmost importance to living a healthy life and require practice and
-          dedication. Our
-          Small Business provides the setting and guidance you need to strengthen your body, mind and soul. All you have
-          to do is
-          show up and breathe.
         </p>
+        <p class="text-light px-md-5 story-text my-3 text-center  text-shadow ">Physical and mental wellbeing are of utmost importance to living a healthy life and require practice and
+        dedication. Our
+        Small Business provides the setting and guidance you need to strengthen your body, mind and soul. All you have
+        to do is
+        show up and breathe.</p>
       </div>
 
     </div>
     <div class="container-fluid mt-5">
 
-      <div class="row mt-5 px-3">
+      <div class="row mt-5 px-md-3">
         <div class="col-md-6 elevation-6">
           <img
             src="https://scontent.fboi1-1.fna.fbcdn.net/v/t39.30808-6/306519061_129159436532870_6494007048333346057_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=ZyXKoZ6J5CIAX-iRhfY&_nc_ht=scontent.fboi1-1.fna&oh=00_AfCPQrcs1-iLVW0CLPjhl0-intGdQOE1_KoCweThnqKaAg&oe=639D65A6"
-            alt="" class="img-fluid framed my-5 ms-md-5 ">
+            alt="" class="img-fluid framed my-5 ms-md-5 " >
         </div>
         <div class="col-md-6">
           <p class="display-3"> Katy Alverson</p>
@@ -83,6 +82,7 @@ export default {
 
 .framed {
   height: 40em;
+  object-fit: cover;
 
   box-shadow:
     -50px -50px 0 -40px var(--darkPink),
