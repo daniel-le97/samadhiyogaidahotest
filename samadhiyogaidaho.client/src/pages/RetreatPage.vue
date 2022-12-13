de1b6a4f85e6a00759d2ed26d4e67beb9ba125cd
TungLe0319 <tung.le0319@gmail.com>
2022-12-13 14:03:43 -0700

    retreats

--- samadhiyogaidaho.client/src/pages/RetreatPage.vue
+++ samadhiyogaidaho.client/src/pages/RetreatPage.vue
@@ -1,27 +1,19 @@
 <template>
-<div class="hero-image d-flex align-items-center justify-content-center">
-  <div class="row">
-    <div class="col-md-12">
-  <h1 class="display-1 courgette text-shadow text-light">    This is the Retreat Page</h1>
-  <ul>
-    <li>
-      <h1 class="display-6 courgette text-shadow text-light">   Current and past Retreats</h1>
-  </li>
-    <li>
-      <h1 class="display-6 courgette text-shadow text-light">  bulleted points with expanding dropdown for more details?</h1>
-  </li>
-  </ul>
+  <div class="hero-image d-flex align-items-center justify-content-center">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="display-1 font-1 text-shadow text-light">Join Our Retreat</h1>
+      </div>
     </div>
   </div>
-</div>
 
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <RetreatForm/>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -29,33 +21,31 @@ import { computed } from "@vue/reactivity";
 import { onMounted, ref, watchEffect } from "vue";
 import { AppState } from "../AppState.js";
 import RetreatForm from "../components/RetreatForm.vue";
-import { logger } from "../utils/Logger.js"
+import { logger } from "../utils/Logger.js";
 import Pop from "../utils/Pop.js";
 
 export default {
-    props: {},
-    setup(props) {
-        const editable = ref({});
-        onMounted(() => {
-        });
-        watchEffect(() => { });
-        return {
-            editable,
-        };
-    },
-    components: { RetreatForm }
-}
+  props: {},
+  setup(props) {
+    const editable = ref({});
+    onMounted(() => {});
+    watchEffect(() => {});
+    return {
+      editable,
+    };
+  },
+  components: { RetreatForm },
+};
 </script>
 
 <style lang="scss" scoped>
-
-.hero-image{
+.hero-image {
   height: 100vh;
   /* always scale the image to the appropriate size of your screen */
   background-size: cover;
   background-image: url(https://images.unsplash.com/photo-1589790272257-8c0f79c221da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80);
   background-position: center;
   /* keeps the image fixed while scrolling , neat effect. */
-  background-attachment: fixed; 
+  background-attachment: fixed;
 }
 </style>
