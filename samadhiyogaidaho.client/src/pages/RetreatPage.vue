94010e8778839afc330f02241f2ffef5a4f59cbd
TungLe0319 <tung.le0319@gmail.com>
2022-12-13 15:26:32 -0700

    tung

--- samadhiyogaidaho.client/src/pages/RetreatPage.vue
+++ samadhiyogaidaho.client/src/pages/RetreatPage.vue
@@ -1,43 +1,91 @@
 <template>
   <section>
-
     <div class="hero-image d-flex align-items-center justify-content-center">
       <div class="row">
         <div class="col-md-12">
-          <h1 class="display-1 font-1 text-shadow text-light">Join Our Retreat</h1>
+          <h1 class="display-1 font-1 text-shadow text-light">
+            Join Our Retreat
+          </h1>
         </div>
       </div>
     </div>
   </section>
 
   <section>
-
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-<h1 class="font-1 text-dark display-2">{{retreat.title}}  </h1>
-<h2 class="font-1 display-6 text-muted"> Sub Title </h2>
+          <h1 class="font-1 text-dark display-2">{{ retreat.title }}</h1>
+          <h2 class="font-1 display-6 text-muted">Sub Title</h2>
         </div>
-
       </div>
 
       <div class="row">
         <div class="col-md-6">
-          <img :src="retreat.coverImg" alt="" class="img-fluid rounded-4 elevation-5 h-100">
+          <img
+            :src="retreat.coverImg"
+            alt=""
+            class="img-fluid rounded-4 elevation-5 h-100"
+          />
         </div>
         <div class="col-md-6">
           <div class="row">
             <div class="col-md-12 mb-3">
-                     <img :src="retreat.coverImg" alt="" class="img-fluid rounded-4 elevation-5">
+              <img
+                :src="retreat.coverImg"
+                alt=""
+                class="img-fluid rounded-4 elevation-5"
+              />
             </div>
             <div class="col-md-6">
-                     <img :src="retreat.coverImg" alt="" class="img-fluid rounded-4 elevation-5">
+              <img
+                :src="retreat.coverImg"
+                alt=""
+                class="img-fluid rounded-4 elevation-5"
+              />
             </div>
             <div class="col-md-6">
-                     <img :src="retreat.coverImg" alt="" class="img-fluid rounded-4 elevation-5">
+              <img
+                :src="retreat.coverImg"
+                alt=""
+                class="img-fluid rounded-4 elevation-5"
+              />
             </div>
           </div>
         </div>
+      </div>
+
+      <div class="row my-5 ">
+  <div class="col-md-8">
+          <ul class="nav nav-pills mb-3 justify-content-center d-flex" id="pills-tab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="summary-tab" data-bs-toggle="pill" data-bs-target="#summary" type="button" role="tab" aria-controls="summary" aria-selected="true">Summary</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="location-tab" data-bs-toggle="pill" data-bs-target="#location " type="button" role="tab" aria-controls="location " aria-selected="false">Location</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="cost-tab" data-bs-toggle="pill" data-bs-target="#cost" type="button" role="tab" aria-controls="cost" aria-selected="false">Cost</button>
+  </li>
+  
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="activities-tab" data-bs-toggle="pill" data-bs-target="#activities" type="button" role="tab" aria-controls="activities" aria-selected="false">Activities</button>
+  </li>
+  
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="summary" role="tabpanel" aria-labelledby="summary-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="location" role="tabpanel" aria-labelledby="location -tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="cost" role="tabpanel" aria-labelledby="cost-tab" tabindex="0">...</div>
+  <div class="tab-pane fade" id="activities" role="tabpanel" aria-labelledby="activities-tab" tabindex="0">...</div>
+ 
+</div>
+  </div>
+  <div class="col-md-4 d-flex justify-content-center">
+    <div class="card border-0 elevation-5 rounded-4 p-4">
+    <button class="btn btn-outline-success"> Price</button>
+    </div>
+  </div>
       </div>
     </div>
   </section>
@@ -57,21 +105,21 @@ export default {
   setup(props) {
     const editable = ref({});
     onMounted(() => {
-      getAllRetreats()
+      getAllRetreats();
     });
     watchEffect(() => {});
 
-    async function getAllRetreats(){
+    async function getAllRetreats() {
       try {
-          await retreatsService.getAllRetreats()
-          console.log(AppState.currentRetreat);
-        } catch (error) {
-          Pop.error(error,'[getCurrentRetreat]')
-        }
+        await retreatsService.getAllRetreats();
+        console.log(AppState.currentRetreat);
+      } catch (error) {
+        Pop.error(error, "[getCurrentRetreat]");
+      }
     }
     return {
       editable,
-      retreat:computed(() => AppState.currentRetreat),
+      retreat: computed(() => AppState.currentRetreat),
     };
   },
   components: { RetreatForm },
@@ -89,7 +137,7 @@ export default {
   background-attachment: fixed;
 }
 
-.img-fluid{
+.img-fluid {
   object-fit: cover;
 }
 </style>
