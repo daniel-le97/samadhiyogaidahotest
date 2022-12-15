9e35c95b25ae6769ca2337c8a833190b1d111d1a
tri le <letrin93@gmail.com>
2022-12-15 16:20:05 -0700

    tri

--- samadhiyogaidaho.client/src/pages/ServicesPage.vue
+++ samadhiyogaidaho.client/src/pages/ServicesPage.vue
@@ -4,61 +4,47 @@
     <div class="row justify-content-center">
       <div class="col-md-6 p-5 d-flex justify-content-center">
         <div class="card rounded-circle text-dark border-0 elevation-5 rounded-1 ">
-          <img
-            class="card-img  rounded-circle"
-            src="http://thiscatdoesnotexist.com"
-            alt="Title"
-          />
+          <img class="card-img  rounded-circle" src="https://images.unsplash.com/photo-1610344264630-e77bc4bf5983?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA3fHx5b2dhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="Title" />
           <div class="card-img-overlay ">
-            <h1 class=""> TESTING</h1>
+            <h1 class="text-center">Book a Private Class for your Work Group</h1>
           </div>
         </div>
       </div>
- 
-   
+
+
       <div class="col-md-6 p-5 d-flex justify-content-center">
         <div class="card rounded-circle text-dark border-0 elevation-5 rounded-1 ">
-          <img
-            class="card-img  rounded-circle"
-            src="http://thiscatdoesnotexist.com"
-            alt="Title"
-          />
+          <img class="card-img  rounded-circle" src="https://images.unsplash.com/photo-1507120410856-1f35574c3b45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="Title" />
           <div class="card-img-overlay ">
-            <h1 class=""> TESTING</h1>
+            <h1 class="text-center">Workshops</h1>
           </div>
         </div>
       </div>
- 
-   
+
+
       <div class="col-md-6 p-5 d-flex justify-content-center">
         <div class="card rounded-circle text-dark border-0 elevation-5 rounded-1 ">
-          <img
-            class="card-img  rounded-circle"
-            src="http://thiscatdoesnotexist.com"
-            alt="Title"
-          />
+          <img class="card-img  rounded-circle" src="https://images.unsplash.com/photo-1499728603263-13726abce5fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="Title" />
           <div class="card-img-overlay ">
-            <h1 class=""> TESTING</h1>
+            <h1 class="text-center">Inclusive Retreats</h1>
           </div>
         </div>
       </div>
- 
-   
+
+
       <div class="col-md-6 p-5 d-flex justify-content-center">
         <div class="card rounded-circle text-dark border-0 elevation-5 rounded-1 ">
-          <img
-            class="card-img  rounded-circle"
+          <img class="card-img  rounded-circle"
             src="https://images.unsplash.com/photo-1636619297905-54f124aa90b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            alt="Title"
-          />
+            alt="Title" />
           <div class="card-img-overlay ">
-            <h1 class=""> TESTING</h1>
+            <h1 class="text-center">Events</h1>
           </div>
         </div>
       </div>
- 
-   
-    
+
+
+
     </div>
   </div>
 </template>
@@ -75,8 +61,8 @@ export default {
   setup(props) {
     const editable = ref({});
 
-    onMounted(() => {});
-    watchEffect(() => {});
+    onMounted(() => { });
+    watchEffect(() => { });
 
     return {
       editable,
@@ -95,29 +81,34 @@ export default {
   /* keeps the image fixed while scrolling , neat effect. */
   background-attachment: fixed;
 }
-.card  img{
-height: 400px;
-width: 400px;
-object-fit:cover ;
-}
 
-.card-img-overlay{
-  opacity: 0;
-    transition: all 0.25s ease;
-
-}
 .card img {
-    transition: all 0.25s ease;  
+  height: 300px;
+  width: 300px;
+  object-fit: cover;
 }
-.card:hover{
-     transition: all 0.25s ease;
-     box-shadow: none;
-background: transparent;
-img{
+
+.card-img-overlay {
   opacity: 0;
-      transition: all 0.25s ease;
+  transition: all 0.25s ease;
+
 }
-  .card-img-overlay{
+
+.card img {
+  transition: all 0.25s ease;
+}
+
+.card:hover {
+  transition: all 0.25s ease;
+  box-shadow: none;
+  background: transparent;
+
+  img {
+    opacity: 0;
+    transition: all 0.25s ease;
+  }
+
+  .card-img-overlay {
     opacity: 1;
     transition: all 0.25s ease;
   }
