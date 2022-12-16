dcf676daef48ad1e9ec4ee6c8e5a2367bb0245e4
Tung N Le <tung.le0319@gmail.com>
2022-12-16 13:55:22 -0700

    tiptap

--- samadhiyogaidaho.client/src/pages/BlogPage.vue
+++ samadhiyogaidaho.client/src/pages/BlogPage.vue
@@ -1,8 +1,8 @@
 <template>
-<section class="container">
+<section class="container" style="margin-top:300px">
   <div class="row">
-    <div class="col-md-example">
-      
+    <div class="col-md-6">
+      <TipTapEditor/>
     </div>
   </div>
 </section>
@@ -12,26 +12,23 @@
 import { computed } from "@vue/reactivity";
 import { onMounted, ref, watchEffect } from "vue";
 import { AppState } from "../AppState.js";
+import TipTapEditor from "../components/TipTapEditor.vue";
 import { logger } from "../utils/Logger.js"
 import Pop from "../utils/Pop.js";
 
 export default {
-props:{
-
-       },
-  setup(props) {
-    const editable = ref({});
-    
-    onMounted(() => {
-
-    });
-    watchEffect(() => {});
-
-    return {
-      editable,
-      }
-    }
-  }
+    props: {},
+    setup(props) {
+        const editable = ref({});
+        onMounted(() => {
+        });
+        watchEffect(() => { });
+        return {
+            editable,
+        };
+    },
+    components: { TipTapEditor }
+}
 </script>
 
 <style lang="scss" scoped>
