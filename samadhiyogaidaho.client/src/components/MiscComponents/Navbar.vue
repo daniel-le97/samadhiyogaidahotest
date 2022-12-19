411281967d26c15233a48da7d60f81cbb8398454
tri le <letrin93@gmail.com>
2022-12-18 21:07:56 -0700

    tri

--- samadhiyogaidaho.client/src/components/MiscComponents/Navbar.vue
+++ samadhiyogaidaho.client/src/components/MiscComponents/Navbar.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav
-
-    class="navbar navbar-expand-lg navbar-dark text-dark  px-3 fixed-top font-2 nav py-5"
-  >
+  <nav class="navbar navbar-expand-lg navbar-dark text-dark  px-3 fixed-top font-2 nav py-5">
     <div class="d-flex mx-auto">
       <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
         <div class="d-flex flex-column align-items-center">
@@ -19,7 +16,7 @@
             height="70"
             class="rounded-circle logo"
           /> -->
-           <!-- <div v-motion-fade class="text-center " style="margin-top:80px;">
+          <!-- <div v-motion-fade class="text-center " style="margin-top:80px;">
           <img
             src="../assets/img/Screenshot_4.png"
             alt=""
@@ -29,80 +26,53 @@
         </div> -->
         </div>
       </router-link>
-     
-      <button
-        class="navbar-toggler btn-dark text-dark bg-dark"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarText"
-        aria-controls="navbarText"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+
+      <button class="navbar-toggler btn-dark text-dark bg-dark" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto">
           <li>
-            <router-link
-              :to="{ name: 'Home' }"
-              v-motion-pop
-              :delay="300"
-              class="btn text-uppercase  fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'Home' }" v-motion-pop :delay="300"
+              class="btn text-uppercase  fw-bold fs-4 nav-link">
               Home
             </router-link>
           </li>
           <li class="">
-            <router-link
-              :to="{ name: 'About' }"
-              v-motion-pop
-              :delay="300"
-              class="btn text-uppercase text-uppercase fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'About' }" v-motion-pop :delay="300"
+              class="btn text-uppercase text-uppercase fw-bold fs-4 nav-link">
               About
             </router-link>
           </li>
           <li>
-            <router-link
-              :to="{ name: 'Retreat' }"
-              v-motion-pop
-              :delay="300"
-              class="btn text-uppercase fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'Retreat' }" v-motion-pop :delay="300"
+              class="btn text-uppercase fw-bold fs-4 nav-link">
               Retreats
             </router-link>
           </li>
           <li>
-            <router-link
-              :to="{ name: 'Services' }"
-              v-motion-pop
-              :delay="300"
-              class="btn text-uppercase fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'Services' }" v-motion-pop :delay="300"
+              class="btn text-uppercase fw-bold fs-4 nav-link">
               Services
             </router-link>
           </li>
-          <li class="mx-5"><div v-motion-fade :delay="1000" class="text-center">
-            <img src="../../assets/img/logo.png" alt="" class="rounded-pill elevation-orange logo-image " />
-          </div></li>
+          <li class="mx-5">
+            <div v-motion-fade :delay="1000" class="text-center">
+              <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+                <img src="../../assets/img/logo.png" alt="" class="rounded-pill elevation-orange logo-image" title="home button" />
+              </router-link>
+            </div>
+          </li>
           <li>
-            <router-link
-              :to="{ name: 'Events' }"
-              v-motion-pop
-              :delay="300"
-              class="btn text-uppercase fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'Events' }" v-motion-pop :delay="300"
+              class="btn text-uppercase fw-bold fs-4 nav-link">
               Events
             </router-link>
           </li>
           <li>
-            <router-link
-              :to="{ name: 'Blog' }"
-              v-motion-pop
-              :delay="300"
-              class="btn text-uppercase fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'Blog' }" v-motion-pop :delay="300"
+              class="btn text-uppercase fw-bold fs-4 nav-link">
               Blog
             </router-link>
           </li>
@@ -117,26 +87,18 @@
             </router-link>
           </li> -->
           <li>
-            <router-link
-              :to="{ name: 'Testimonials' }"
-              v-motion-pop
-              :delay="300"
-              class="btn  text-uppercase fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'Testimonials' }" v-motion-pop :delay="300"
+              class="btn  text-uppercase fw-bold fs-4 nav-link">
               Testimonials
             </router-link>
           </li>
           <li>
-            <router-link
-              :to="{ name: 'Contact' }"
-              v-motion-pop
-              :delay="300"
-              class="btn text-uppercase fw-bold fs-4 nav-link"
-            >
+            <router-link :to="{ name: 'Contact' }" v-motion-pop :delay="300"
+              class="btn text-uppercase fw-bold fs-4 nav-link">
               Connect
             </router-link>
           </li>
-       
+
         </ul>
         <!-- LOGIN COMPONENT HERE -->
         <!-- <Login /> -->
@@ -162,27 +124,28 @@ export default {
 <style scoped>
 .nav-link {
   text-transform: uppercase;
-  color:#ffffff;
+  color: #ffffff;
   margin-left: 8px;
   margin-right: 8px;
-    text-shadow: 0px 1px 4px #000000;
+  text-shadow: 0px 1px 4px #000000;
 }
+
 .nav-link:hover {
   text-transform: uppercase;
   color: var(--darkPink) ! important
 }
 
-.nav{
-       background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 100%, rgba(0, 0, 0, 0.7) 75%, rgba(0, 0, 0, 0.134) 0%);
-       
+.nav {
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 100%, rgba(0, 0, 0, 0.7) 75%, rgba(0, 0, 0, 0.134) 0%);
+
 }
 
-.logo-image{
+.logo-image {
   width: 100px;
   height: 160px;
 }
 
-.logo{
+.logo {
   object-fit: cover;
 
 }
