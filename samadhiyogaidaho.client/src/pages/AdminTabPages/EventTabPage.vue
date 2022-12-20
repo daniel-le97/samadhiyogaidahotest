1e0ae4d5a8ec0f789f025ab7c8d7ca0f1b8a3f59
daniel <daniel97.le@gmail.com>
2022-12-20 14:21:14 -0700

    crud

--- samadhiyogaidaho.client/src/pages/AdminTabPages/EventTabPage.vue
+++ samadhiyogaidaho.client/src/pages/AdminTabPages/EventTabPage.vue
@@ -1,32 +1,54 @@
 <template>
-  <div class="component">
+  <div class="container-fluid">
+    <div class="row flex-wrap">
+      <div class="col-12" v-for="event in events">
+        <div class="elevation-1 eventCard">
+          <EventCard :event="event" />
+        </div>
+      </div>
+    </div>
     <button @click="form = !form" class="btn btn-primary">add an event</button>
     <button @click="form = !form" class="btn btn-primary">edit an event</button>
-    
 
-
-
-    <EventForm v-if="form"/>
+    <EventForm v-if="form" />
   </div>
 </template>
 
-
 <script>
-import { onMounted, ref } from 'vue';
-import EventForm from '../../components/AdminComponents/EventForm.vue';
+import { getEventListeners } from "events";
+import { onMounted, ref } from "vue";
+import { AppState } from "../../AppState";
+import EventForm from "../../components/AdminComponents/EventForm.vue";
+import EventCard from "../../components/EventsPage/EventCard.vue";
+import { eventsService } from "../../services/EventsService";
+import Pop from "../../utils/Pop";
+import { computed } from "@vue/reactivity";
 
 export default {
-    setup() {
-      const form = ref(false)
-        return {
-          form
-        };
-    },
-    components: { EventForm }
-}
+  setup() {
+    onMounted(() => {
+      getEvents();
+    });
+    async function getEvents() {
+      try {
+        await eventsService.getEvents();
+      } catch (error) {
+        Pop.error(error);
+      }
+    }
+    const form = ref(false);
+    return {
+      events: computed(() => AppState.events),
+      form,
+    };
+  },
+  components: { EventForm, EventCard },
+};
 </script>
 
-
 <style lang="scss" scoped>
-
+// .eventCard {
+//   height: 25vh;
+//   overflow-y: auto;
+// }
 </style>
