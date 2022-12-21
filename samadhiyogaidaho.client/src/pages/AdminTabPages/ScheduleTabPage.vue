ebf6cdbacd891df43a4f7ab45427f4c1245762aa
daniel <daniel97.le@gmail.com>
2022-12-21 10:40:10 -0700

    change

--- samadhiyogaidaho.client/src/pages/AdminTabPages/ScheduleTabPage.vue
+++ samadhiyogaidaho.client/src/pages/AdminTabPages/ScheduleTabPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="component">
-
+<ScheduleSection/>
 
   </div>
 </template>
 
 
 <script>
+import ScheduleSection from '../../components/HomePage/ScheduleSection.vue';
+
 export default {
-  setup(){
-    return {}
-  }
+    setup() {
+        return {};
+    },
+    components: { ScheduleSection }
 }
 </script>
 
