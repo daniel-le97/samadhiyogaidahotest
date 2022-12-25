168cfdc088bb7ced40b6339c6f4e8f85fe87fccd
daniel <daniel97.le@gmail.com>
2022-12-24 17:46:08 -0700

    1

--- samadhiyogaidaho.client/src/services/pocketBaseService.js
+++ samadhiyogaidaho.client/src/services/pocketBaseService.js
@@ -12,26 +12,19 @@ class PocketBaseService {
     const imgs = [];
     const files = Array.from(e.target.files);
     // console.log(files);
-    for await(const file of files) {
-      let reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = e =>{
-      imgs.push(e)
-    }
-  }
-  console.log(imgs);
 
-    // for await (const file of files) {
-    //   let formData = new FormData()
-    //   formData.append("file", file)
-    //   const createdFile = await pb.collection('images').create(formData)
-    //   const url = await pb.getFileUrl(createdFile, createdFile.file)
-    //   const getLastFile = await pb.collection("images").update(createdFile.id, {url})
-    //   imgs.push(url);
-    // }
-    // // await pb.collection("images").subscribe("*",function(data))
-    // logger.log(imgs)
-    // return imgs;
+
+    for await (const file of files) {
+      let formData = new FormData()
+      formData.append("file", file)
+      const createdFile = await pb.collection('images').create(formData)
+      const url = await pb.getFileUrl(createdFile, createdFile.file)
+      const getLastFile = await pb.collection("images").update(createdFile.id, {url})
+      imgs.push(url);
+    }
+    // await pb.collection("images").subscribe("*",function(data))
+    logger.log(imgs)
+    return imgs;
   }
 
   async getPastRetreats() {
