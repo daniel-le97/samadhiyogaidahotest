507f4c5a006b6f470a4e9762053d1406ab5f5857
Tung N Le <tung.le0319@gmail.com>
2023-01-08 15:13:36 -0700

    tung

--- samadhiyogaidaho.client/src/components/HomePage/ScheduleSection.vue
+++ samadhiyogaidaho.client/src/components/HomePage/ScheduleSection.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container schedule-section my-5">
-    <div class="row" >
+  <div class="container-fluid schedule-section my-5">
+    <div class="row position-relative" >
       <div class="col-md-12 text-center">
         <h1
           v-motion-slide-visible-once-bottom
@@ -12,7 +12,7 @@
         <br />
       </div>
 
-      <div class="col-md-6 d-flex align-items-center justify-content-center">
+      <div class="col-md-8 d-flex align-items-center justify-content-center">
         <div
           v-motion-slide-visible-once-bottom
           :delay="300"
@@ -22,13 +22,13 @@
             <img
               src="../../assets/img/studio1.jpg"
               alt=""
-              class="img-fluid rounded-4 elevation-6 schedule-image"
+              class="img-fluid rounded-4 elevation-6 schedule-img"
             />
           </div>
         </div>
       </div>
       <div
-        class="col-md-6 d-flex flex-column align-content-center justify-content-center"
+        class="col-md-6 schedule-card rounded elevation-6 bg-white d-flex flex-column align-content-center justify-content-center"
       >
         <ScheduleCard :schedule="s" v-for="s in schedules" />
       </div>
@@ -58,4 +58,34 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+.schedule-img{
+width: 100%;
+object-fit: cover;
+}
+.schedule-card{
+  position: absolute;
+  left: 600px;
+  top: 300px;
+  //when screen is 768px OR LESS
+  //when screen is 768px OR LESS
+  //when screen is 768px OR LESS
+  @media only screen and (max-width: 1550px){
+    width: 600px;
+      left: 650px;
+     top: 300px;
+  }
+  @media only screen and (max-width: 1200px){
+      position: absolute;
+    left: 400px;
+  top: 300px;
+  }
+  @media only screen and (max-width: 768px){
+  position: static;
+  }
+}
+
+
+
+</style>
