3304ea7282e4ed0db299d54e63e221aef545235a
Tung N Le <tung.le0319@gmail.com>
2023-01-17 11:51:30 -0700

    fIX daniel's mistake

--- samadhiyogaidaho.client/src/services/NewsLetterService.js
+++ samadhiyogaidaho.client/src/services/NewsLetterService.js
@@ -5,7 +5,7 @@ import { api, ghostAPI } from "./AxiosService.js";
 class NewsLetterService {
   async addNewsLetterSubscription(data) {
     const res = await ghostAPI.post("/api/send-magic-link/", data);
-    console.log(res);
+    // console.log(res);
     // TODO check ghost cms to see if this works
   }
 }
